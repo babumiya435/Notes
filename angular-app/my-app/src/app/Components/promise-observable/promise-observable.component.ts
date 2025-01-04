91a426a4231375885cc0143b463de77cbfc24a00
moba0222 <mohammad.babu.miya@netcracker.com>
2025-01-04 14:37:57 +0530

    practice1

---
+++ angular-app/my-app/src/app/Components/promise-observable/promise-observable.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-promise-observable',
+  standalone: true,
+  imports: [],
+  templateUrl: './promise-observable.component.html',
+  styleUrl: './promise-observable.component.css'
+})
+export class PromiseObservableComponent {
+
+}
