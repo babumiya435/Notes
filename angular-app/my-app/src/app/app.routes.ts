391bf38d91b78400d648215e96f5437e70b6ed5d
moba0222 <mohammad.babu.miya@netcracker.com>
2025-01-05 10:35:16 +0530

    practice1

--- angular-app/my-app/src/app/app.routes.ts
+++ angular-app/my-app/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { PromiseObservableComponent } from './Components/promise-observable/promise-observable.component';
 import { PageNotFoundComponent } from './Components/page-not-found/page-not-found.component';
+import { StoreComponent } from './Components/store/store.component';
 
 export const routes: Routes = [
     { path : '', redirectTo : '/promise', pathMatch : 'full' },
@@ -14,6 +15,12 @@ export const routes: Routes = [
         path: 'promise',
         loadComponent: () => import('./Components/promise-observable/promise-observable.component').then(m => m.PromiseObservableComponent),
         title: 'promise-observable'
+    },
+    // { path : 'store', component : StoreComponent },
+    {
+        path : 'store',
+        loadComponent : () => import('./Components/store/store.component').then(m => m.StoreComponent),
+        title : 'store'
     },
     { path : '**', component : PageNotFoundComponent },
     // {
